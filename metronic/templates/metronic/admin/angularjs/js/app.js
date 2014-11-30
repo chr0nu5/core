8aa7a29543d7202bd22caa615e0b5a228f26f724
Jonathan Querubina <jonathan@phocus.com.br>
2014-11-30 08:47:07 -0200

    url fix for templates

--- metronic/templates/metronic/admin/angularjs/js/app.js
+++ metronic/templates/metronic/admin/angularjs/js/app.js
@@ -104,15 +104,15 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load({
                         name: 'MetronicApp',
                         files: [
-                            '../../../assets/global/plugins/morris/morris.css',
-                            '../../../assets/admin/pages/css/tasks.css',
+                            '../../../../static/metronic/global/plugins/morris/morris.css',
+                            '../../../../static/metronic/admin/pages/css/tasks.css',
                             
-                            '../../../assets/global/plugins/morris/morris.min.js',
-                            '../../../assets/global/plugins/morris/raphael-min.js',
-                            '../../../assets/global/plugins/jquery.sparkline.min.js',
+                            '../../../../static/metronic/global/plugins/morris/morris.min.js',
+                            '../../../../static/metronic/global/plugins/morris/raphael-min.js',
+                            '../../../../static/metronic/global/plugins/jquery.sparkline.min.js',
 
-                            '../../../assets/admin/pages/scripts/index3.js',
-                            '../../../assets/admin/pages/scripts/tasks.js',
+                            '../../../../static/metronic/admin/pages/scripts/index3.js',
+                            '../../../../static/metronic/admin/pages/scripts/tasks.js',
 
                              'js/controllers/DashboardController.js'
                         ] 
@@ -132,7 +132,7 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load([{
                         name: 'angularFileUpload',
                         files: [
-                            '../../../assets/global/plugins/angularjs/plugins/angular-file-upload/angular-file-upload.min.js',
+                            '../../../../static/metronic/global/plugins/angularjs/plugins/angular-file-upload/angular-file-upload.min.js',
                         ] 
                     }, {
                         name: 'MetronicApp',
@@ -155,8 +155,8 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load([{
                         name: 'ui.select',
                         files: [
-                            '../../../assets/global/plugins/angularjs/plugins/ui-select/select.min.css',
-                            '../../../assets/global/plugins/angularjs/plugins/ui-select/select.min.js'
+                            '../../../../static/metronic/global/plugins/angularjs/plugins/ui-select/select.min.css',
+                            '../../../../static/metronic/global/plugins/angularjs/plugins/ui-select/select.min.js'
                         ] 
                     }, {
                         name: 'MetronicApp',
@@ -197,10 +197,10 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load([{
                         name: 'MetronicApp',
                         files: [
-                            '../../../assets/global/plugins/jstree/dist/themes/default/style.min.css',
+                            '../../../../static/metronic/global/plugins/jstree/dist/themes/default/style.min.css',
 
-                            '../../../assets/global/plugins/jstree/dist/jstree.min.js',
-                            '../../../assets/admin/pages/scripts/ui-tree.js',
+                            '../../../../static/metronic/global/plugins/jstree/dist/jstree.min.js',
+                            '../../../../static/metronic/admin/pages/scripts/ui-tree.js',
                             'js/controllers/GeneralPageController.js'
                         ] 
                     }]);
@@ -219,24 +219,24 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load([{
                         name: 'MetronicApp',
                         files: [
-                            '../../../assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css',
-                            '../../../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css',
-                            '../../../assets/global/plugins/jquery-tags-input/jquery.tagsinput.css',
-                            '../../../assets/global/plugins/bootstrap-markdown/css/bootstrap-markdown.min.css',
-                            '../../../assets/global/plugins/typeahead/typeahead.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css',
+                            '../../../../static/metronic/global/plugins/jquery-tags-input/jquery.tagsinput.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-markdown/css/bootstrap-markdown.min.css',
+                            '../../../../static/metronic/global/plugins/typeahead/typeahead.css',
 
-                            '../../../assets/global/plugins/fuelux/js/spinner.min.js',
-                            '../../../assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js',
-                            '../../../assets/global/plugins/jquery-inputmask/jquery.inputmask.bundle.min.js',
-                            '../../../assets/global/plugins/jquery.input-ip-address-control-1.0.min.js',
-                            '../../../assets/global/plugins/bootstrap-pwstrength/pwstrength-bootstrap.min.js',
-                            '../../../assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
-                            '../../../assets/global/plugins/jquery-tags-input/jquery.tagsinput.min.js',
-                            '../../../assets/global/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js',
-                            '../../../assets/global/plugins/bootstrap-touchspin/bootstrap.touchspin.js',
-                            '../../../assets/global/plugins/typeahead/handlebars.min.js',
-                            '../../../assets/global/plugins/typeahead/typeahead.bundle.min.js',
-                            '../../../assets/admin/pages/scripts/components-form-tools.js',
+                            '../../../../static/metronic/global/plugins/fuelux/js/spinner.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js',
+                            '../../../../static/metronic/global/plugins/jquery-inputmask/jquery.inputmask.bundle.min.js',
+                            '../../../../static/metronic/global/plugins/jquery.input-ip-address-control-1.0.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-pwstrength/pwstrength-bootstrap.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
+                            '../../../../static/metronic/global/plugins/jquery-tags-input/jquery.tagsinput.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-touchspin/bootstrap.touchspin.js',
+                            '../../../../static/metronic/global/plugins/typeahead/handlebars.min.js',
+                            '../../../../static/metronic/global/plugins/typeahead/typeahead.bundle.min.js',
+                            '../../../../static/metronic/admin/pages/scripts/components-form-tools.js',
 
                             'js/controllers/GeneralPageController.js'
                         ] 
@@ -256,22 +256,22 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load([{
                         name: 'MetronicApp',
                         files: [
-                            '../../../assets/global/plugins/clockface/css/clockface.css',
-                            '../../../assets/global/plugins/bootstrap-datepicker/css/datepicker3.css',
-                            '../../../assets/global/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css',
-                            '../../../assets/global/plugins/bootstrap-colorpicker/css/colorpicker.css',
-                            '../../../assets/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css',
-                            '../../../assets/global/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css',
+                            '../../../../static/metronic/global/plugins/clockface/css/clockface.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-datepicker/css/datepicker3.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-colorpicker/css/colorpicker.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css',
 
-                            '../../../assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
-                            '../../../assets/global/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js',
-                            '../../../assets/global/plugins/clockface/js/clockface.js',
-                            '../../../assets/global/plugins/bootstrap-daterangepicker/moment.min.js',
-                            '../../../assets/global/plugins/bootstrap-daterangepicker/daterangepicker.js',
-                            '../../../assets/global/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js',
-                            '../../../assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js',
+                            '../../../../static/metronic/global/plugins/clockface/js/clockface.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-daterangepicker/moment.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-daterangepicker/daterangepicker.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js',
 
-                            '../../../assets/admin/pages/scripts/components-pickers.js',
+                            '../../../../static/metronic/admin/pages/scripts/components-pickers.js',
 
                             'js/controllers/GeneralPageController.js'
                         ] 
@@ -291,15 +291,15 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load([{
                         name: 'MetronicApp',
                         files: [
-                            '../../../assets/global/plugins/bootstrap-select/bootstrap-select.min.css',
-                            '../../../assets/global/plugins/select2/select2.css',
-                            '../../../assets/global/plugins/jquery-multi-select/css/multi-select.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-select/bootstrap-select.min.css',
+                            '../../../../static/metronic/global/plugins/select2/select2.css',
+                            '../../../../static/metronic/global/plugins/jquery-multi-select/css/multi-select.css',
 
-                            '../../../assets/global/plugins/bootstrap-select/bootstrap-select.min.js',
-                            '../../../assets/global/plugins/select2/select2.min.js',
-                            '../../../assets/global/plugins/jquery-multi-select/js/jquery.multi-select.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-select/bootstrap-select.min.js',
+                            '../../../../static/metronic/global/plugins/select2/select2.min.js',
+                            '../../../../static/metronic/global/plugins/jquery-multi-select/js/jquery.multi-select.js',
 
-                            '../../../assets/admin/pages/scripts/components-dropdowns.js',
+                            '../../../../static/metronic/admin/pages/scripts/components-dropdowns.js',
 
                             'js/controllers/GeneralPageController.js'
                         ] 
@@ -319,13 +319,13 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load({
                         name: 'MetronicApp',
                         files: [
-                            '../../../assets/global/plugins/select2/select2.css',                             
-                            '../../../assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css', 
-                            '../../../assets/global/plugins/datatables/extensions/Scroller/css/dataTables.scroller.min.css',
-                            '../../../assets/global/plugins/datatables/extensions/ColReorder/css/dataTables.colReorder.min.css',
+                            '../../../../static/metronic/global/plugins/select2/select2.css',                             
+                            '../../../../static/metronic/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css', 
+                            '../../../../static/metronic/global/plugins/datatables/extensions/Scroller/css/dataTables.scroller.min.css',
+                            '../../../../static/metronic/global/plugins/datatables/extensions/ColReorder/css/dataTables.colReorder.min.css',
 
-                            '../../../assets/global/plugins/select2/select2.min.js',
-                            '../../../assets/global/plugins/datatables/all.min.js',
+                            '../../../../static/metronic/global/plugins/select2/select2.min.js',
+                            '../../../../static/metronic/global/plugins/datatables/all.min.js',
                             'js/scripts/table-advanced.js',
 
                             'js/controllers/GeneralPageController.js'
@@ -346,15 +346,15 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load({
                         name: 'MetronicApp',
                         files: [
-                            '../../../assets/global/plugins/select2/select2.css',                             
-                            '../../../assets/global/plugins/bootstrap-datepicker/css/datepicker.css',
-                            '../../../assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css',
+                            '../../../../static/metronic/global/plugins/select2/select2.css',                             
+                            '../../../../static/metronic/global/plugins/bootstrap-datepicker/css/datepicker.css',
+                            '../../../../static/metronic/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css',
 
-                            '../../../assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
-                            '../../../assets/global/plugins/select2/select2.min.js',
-                            '../../../assets/global/plugins/datatables/all.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
+                            '../../../../static/metronic/global/plugins/select2/select2.min.js',
+                            '../../../../static/metronic/global/plugins/datatables/all.min.js',
 
-                            '../../../assets/global/scripts/datatable.js',
+                            '../../../../static/metronic/global/scripts/datatable.js',
                             'js/scripts/table-ajax.js',
 
                             'js/controllers/GeneralPageController.js'
@@ -375,14 +375,14 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load({
                         name: 'MetronicApp',  
                         files: [
-                            '../../../assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css',
-                            '../../../assets/admin/pages/css/profile.css',
-                            '../../../assets/admin/pages/css/tasks.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css',
+                            '../../../../static/metronic/admin/pages/css/profile.css',
+                            '../../../../static/metronic/admin/pages/css/tasks.css',
                             
-                            '../../../assets/global/plugins/jquery.sparkline.min.js',
-                            '../../../assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js',
+                            '../../../../static/metronic/global/plugins/jquery.sparkline.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js',
 
-                            '../../../assets/admin/pages/scripts/profile.js',
+                            '../../../../static/metronic/admin/pages/scripts/profile.js',
 
                             'js/controllers/UserProfileController.js'
                         ]                    
@@ -423,14 +423,14 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     return $ocLazyLoad.load({ 
                         name: 'MetronicApp',  
                         files: [
-                            '../../../assets/global/plugins/bootstrap-datepicker/css/datepicker3.css',
-                            '../../../assets/global/plugins/select2/select2.css',
-                            '../../../assets/admin/pages/css/todo.css',
+                            '../../../../static/metronic/global/plugins/bootstrap-datepicker/css/datepicker3.css',
+                            '../../../../static/metronic/global/plugins/select2/select2.css',
+                            '../../../../static/metronic/admin/pages/css/todo.css',
                             
-                            '../../../assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
-                            '../../../assets/global/plugins/select2/select2.min.js',
+                            '../../../../static/metronic/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js',
+                            '../../../../static/metronic/global/plugins/select2/select2.min.js',
 
-                            '../../../assets/admin/pages/scripts/todo.js',
+                            '../../../../static/metronic/admin/pages/scripts/todo.js',
 
                             'js/controllers/TodoController.js'  
                         ]                    
